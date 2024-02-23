2142601bf268fadd44fd2d3993f2cd6473594b29
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-23 20:09:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function highlight() {
     //Write your code here
-
-
+	// let ptt = document.getElementsByTagName("a");
+	// let ptt = document.querySelector(p);
+	// let ptt = document.getElementById("pt");
+	// ptt.addEventListener("hover",()=>{
+	// 	let col = document.getElementsByTagName("strong");
+	// 	col.style.color = 'green';
+	// });
+	let strongElements = document.querySelectorAll("p strong");
+	// strongElements[6].style.color = 'green';
+	
+	for(var i = 0 ; i<strongElements.length ; i++)
+	{
+		strongElements[i].style.color = 'green';
+	}
 }
 
 
 function return_normal() {
     //Write your code here
-
-    
+	let strongElements = document.querySelectorAll("p strong");
+	for(var i = 0 ; i<strongElements.length ; i++)
+	{
+		strongElements[i].style.color = 'black';
+	}
 }
